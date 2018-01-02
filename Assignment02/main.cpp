afb2640acfe9e83eb69f953b1c2b24eea53c4331
Christian Cataldo <christian.cataldo@itcollege.ee>
2018-01-02 19:53:19 +0200

    Add pizza list

--- Assignment02/main.cpp
+++ Assignment02/main.cpp
@@ -3,7 +3,9 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <string>
 
+#include <jsoncpp/json/json.h>
 #include "json.hpp"
 
 using namespace std;
@@ -21,6 +23,7 @@ int main(){
 				printList();
 				break;
 		}
+	cout << endl;
 	menu = getMenu();
 	}
 }
@@ -28,23 +31,33 @@ int main(){
 // print list function
 int printList(){
 	ifstream recipes{ "recipes.json" };
+	Json::Reader reader;
+    Json::Value obj;
+    reader.parse(recipes, obj); 
 	
 	string name;
+	//string namep = simpleJson["name"];
 	string ingredients;
+	std::vector<std::string> values;
 	
 	cout << endl;
 	cout << "List of pizzas: " << endl;
-	while (recipes >> name >> ingredients){
-		cout << name << ingredients << endl;
-		}
+	for (Json::Value::ArrayIndex i = 0; i != obj.size(); i++)
+    if (obj[i].isMember("name"))
+        cout << obj[i]["name"].asString() << endl;
+	
+	//while (simpleJson >> name >> ingredients){
+		//cout << obj["name"].asString()  << endl;
+	//}
 	cout << endl;
 }
 	
 // menu function
 int getMenu(){
+	cout << "MENU: " << endl;
 	int choice;
 	cout << "1 - print list of pizzas" << endl;
-	cout << "2 - aaaa" << endl;
+	cout << "2 - type the name of the pizza to buy" << endl;
 	cout << "3 - aaaa" << endl;
 	cout << "4 - exit program" << endl;
 	
