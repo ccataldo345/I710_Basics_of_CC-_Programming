498bd2aa8c9c4dbef1a6fdfef908a132142adcb2
Christian Cataldo <christian.cataldo@itcollege.ee>
2018-01-03 18:11:20 +0200

    Add check on pizza to buy

--- Assignment02/main.cpp
+++ Assignment02/main.cpp
@@ -51,7 +51,25 @@ int inputPizzaName(){
 	string pizzaToBuy;
 	cout << "Type the name of the pizza to buy:" << endl;
 	cin >> pizzaToBuy;
-	cout << "You want to buy a pizza " << pizzaToBuy << endl;
+	
+	ifstream recipes{ "recipes.json" };
+	Json::Reader reader;
+    Json::Value obj;
+    reader.parse(recipes, obj); 
+	
+	bool check = false;
+	for (Json::Value::ArrayIndex i = 0; i != obj.size(); i++) {
+		if (obj[i].isMember("name")) {
+			if (obj[i]["name"].asString() == pizzaToBuy) {
+				check = true;
+				break;
+			}
+		}
+	}
+	if (check == false)
+		cout << "This pizza is not in the list, please try again";
+	else 
+		cout << "You want to buy a pizza " << pizzaToBuy << endl;
 	cout << endl;
 }	
 
